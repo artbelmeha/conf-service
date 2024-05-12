60d9e73f32ededbd66285d62583183f5f537ee36
Artem Belmeha <artem_belmeha@epam.com>
2024-05-12 22:24:55 +0300

    Initial commit

---
+++ src/test/java/com/example/confservice/ConfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.confservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
